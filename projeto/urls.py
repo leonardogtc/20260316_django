782f447b23de71174a739f02fe3c97c5f3df03a1
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-03-18 12:58:08 -0300

    Criando, conhecendo e entendendo apps do Django

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
+
 ]
