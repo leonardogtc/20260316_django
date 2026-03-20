b087e34cf82edb207a2a4f23de657add2999784e
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-03-20 20:21:40 -0300

    Ajustando estilo CSS da Header

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name':'Leonardo Tavares'
-    })
+    return render(request, 'recipes/pages/home.html')
 
 
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
