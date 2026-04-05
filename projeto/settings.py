01c23dc1bb611af384cd59529545f6d82512536f
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-05 08:36:52 -0300

    Adicionando recipes - configurando pasta media e link de visualização de imagens no django admin.

--- projeto/settings.py
+++ projeto/settings.py
@@ -125,5 +125,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
